3ecea7253b4c2703a25affb7f0f2d7ec192d264a
bruh-code <djfrizer1@gmail.com>
2017-12-01 08:53:17 -0300

    GET /todos added

--- server/server.js
+++ server/server.js
@@ -20,6 +20,14 @@ app.post('/todos', (req, res) => {
   });
 });
 
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (err) => {
+    res.status(400).send(err);
+  });
+});
+
 var port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
