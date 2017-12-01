d38ce9bc614fe4ebff3a3c65eafe2d7023ce04ca
bruh-code <djfrizer1@gmail.com>
2017-12-01 13:01:09 -0300

    Added tests for DELETE /todo/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -94,3 +94,39 @@ describe('GET /todos/:id', () => {
     .end(done);
   });
 });
+
+describe('DELETE /todos/:id', () => {
+    it('remove a todo', (done) => {
+      var hexId = todos[1]._id.toHexString();
+
+      request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo._id).toBe(hexId);
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        Todo.findById(hexId).then((todo) => {
+          expect(todo).toNotExist();
+          done();
+        }).catch((e) => done(e));
+      });
+    });
+
+    it('return 404 if todo not found', (done) => {
+      request(app)
+      .get(`/todos/${new ObjectID().toHexString()}`)
+      .expect(404)
+      .end(done);
+    });
+
+    it('return 404 if object id is invalid', (done) => {
+      request(app)
+      .get(`/todos/abc`)
+      .expect(404)
+      .end(done);
+    });
+});
