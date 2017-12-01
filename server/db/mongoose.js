822ca868922f25835e5588c0118a1de7eeb2526a
bruh-code <djfrizer1@gmail.com>
2017-12-01 13:51:53 -0300

    Setup env vars

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise; // set mongoose to use js built in promises
 
-const mongoUri = process.env.MONGO_URI || 'mongodb://localhost:27017/TodoApp';
+const mongoUri = process.env.MONGO_URI;
 
 mongoose.connect(mongoUri, {useMongoClient: true}).then(() => {
   console.log(`Sucessfully connected to database ${mongoUri}`);
--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+require('./config/config');
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('lodash');
@@ -86,7 +87,7 @@ app.patch('/todos/:id', (req, res) => {
   });
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
