d3cda61f40e05a392ffeb2f6a7530fb333152940
bruh-code <djfrizer1@gmail.com>
2017-12-01 12:19:31 -0300

    Fixed for using env variable on heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,19 +2,12 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise; // set mongoose to use js built in promises
 
-const dbUser = 'admin';
-const dbPass = '339955';
-const remoteMongoUri = `mongodb://${dbUser}:${dbPass}@ds125146.mlab.com:25146/todoapp`;
+const mongoUri = process.env.MONGO_URI || 'mongodb://localhost:27017';
 
-mongoose.connect(remoteMongoUri, {useMongoClient: true}).then(() => {
-  console.log('Sucessfully connected to remote database');
+mongoose.connect(mongoUri, {useMongoClient: true}).then(() => {
+  console.log('Sucessfully connected to database');
 }).catch((err) => {
-  console.log('Remote database failed, switch to local instead.');
-  mongoose.connect('mongodb://localhost:27017/TodoApp', {useMongoClient: true}).then(() => {
-    console.log('Sucessfully connected to local database');
-  }).catch((err) => {
-    console.log('Error connecting to local database, exiting app');
-  });
+
 });
 
 module.exports.mongoose = mongoose;
