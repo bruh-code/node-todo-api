a351d0e886ae676070ce0c440d9ff638646b797e
bruh-code <djfrizer1@gmail.com>
2017-12-07 10:17:53 -0300

    Added POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -79,7 +79,6 @@ UserSchema.methods.generateAuthToken = function() {
 };
 
 /**
- * New method for User model.
  * Finds an user with the given token, verifying that
  * the token is authentic with jwt.verify and then
  * querying for the user in the database.
@@ -105,7 +104,26 @@ UserSchema.statics.findByToken = function(token) {
     'tokens.token': token,
     'tokens.access': 'auth'
   });
+};
 
+UserSchema.statics.findByCredentials = function(email, password) {
+  var User = this;
+
+  return User.findOne({email}).then((user) => {
+    if(!user) {
+      return Promise.reject();
+    }
+
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+        if(res) {
+          resolve(user);
+        } else {
+          reject();
+        }
+      });
+    });
+  });
 };
 
 /**
