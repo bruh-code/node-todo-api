1535bcabc2e280e97c40bf6d8558a4538b54319c
bruh-code <djfrizer1@gmail.com>
2017-12-14 11:39:23 -0300

    Config.js modificado para usar config.json

--- server/models/user.js
+++ server/models/user.js
@@ -14,32 +14,32 @@ const bcrypt = require('bcryptjs');
  * @type {mongoose}
  */
 var UserSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    minlength: 3,
-    trim: true,
-    unique: true,
-    validate: {
-      validator: validator.isEmail,
-      message: '{VALUE} is not a valid email'
-    }
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 6
-  },
-  tokens: [{
-    access: {
-      type: String,
-      required: true
-    },
-    token: {
-      type: String,
-      required: true
-    }
-  }]
+	email: {
+		type: String,
+		required: true,
+		minlength: 3,
+		trim: true,
+		unique: true,
+		validate: {
+			validator: validator.isEmail,
+			message: '{VALUE} is not a valid email'
+		}
+	},
+	password: {
+		type: String,
+		required: true,
+		minlength: 6
+	},
+	tokens: [{
+		access: {
+			type: String,
+			required: true
+		},
+		token: {
+			type: String,
+			required: true
+		}
+	}]
 });
 
 /**
@@ -49,10 +49,10 @@ var UserSchema = new mongoose.Schema({
  * @return {[Array]} Array with only _id and email properties of userObject
  */
 UserSchema.methods.toJSON = function() {
-  var user = this;
-  var userObject = user.toObject();
+	var user = this;
+	var userObject = user.toObject();
 
-  return _.pick(userObject, ['_id', 'email']);
+	return _.pick(userObject, ['_id', 'email']);
 };
 
 /**
@@ -63,29 +63,29 @@ UserSchema.methods.toJSON = function() {
  * @return {Promise} Promise that saves the user's token.
  */
 UserSchema.methods.generateAuthToken = function() {
-  var user = this;
-  var access = 'auth';
-  var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123');
+	var user = this;
+	var access = 'auth';
+	var token = jwt.sign({_id: user._id.toHexString(), access}, process.env.JWT_SECRET);
 
-  user.tokens.push({
-    access,
-    token
-  });
+	user.tokens.push({
+		access,
+		token
+	});
 
-  // return promise to allow server.js to chain into it
-  return user.save().then(() => {
-    return token;
-  });
+	// return promise to allow server.js to chain into it
+	return user.save().then(() => {
+		return token;
+	});
 };
 
 UserSchema.methods.removeToken = function(token) {
-  var user = this;
+	var user = this;
 
-  return user.update({
-    $pull: {
-      tokens: {token}
-    }
-  });
+	return user.update({
+		$pull: {
+			tokens: {token}
+		}
+	});
 };
 
 /**
@@ -96,24 +96,24 @@ UserSchema.methods.removeToken = function(token) {
  * @return {Promise}      A reject Promise if token is not authentic or a findOne Promise with a possible user
  */
 UserSchema.statics.findByToken = function(token) {
-  var User = this; // user model
-  var decoded;
+	var User = this; // user model
+	var decoded;
 
-  try {
-    decoded = jwt.verify(token, 'abc123');
-  } catch (err) {
-    // return new Promise((resolve, reject) => {
-    //   reject();
-    // });
-    // code below does same thing
-    return Promise.reject();
-  }
+	try {
+		decoded = jwt.verify(token, process.env.JWT_SECRET);
+	} catch (err) {
+		// return new Promise((resolve, reject) => {
+		//   reject();
+		// });
+		// code below does same thing
+		return Promise.reject();
+	}
 
-  return User.findOne({
-    '_id': decoded._id,
-    'tokens.token': token,
-    'tokens.access': 'auth'
-  });
+	return User.findOne({
+		'_id': decoded._id,
+		'tokens.token': token,
+		'tokens.access': 'auth'
+	});
 };
 
 /**
@@ -126,23 +126,23 @@ UserSchema.statics.findByToken = function(token) {
  * @return {Promise} A Promise with the user found. If user is not found or password is not correct it returns a reject Promise.
  */
 UserSchema.statics.findByCredentials = function(email, password) {
-  var User = this;
+	var User = this;
 
-  return User.findOne({email}).then((user) => {
-    if(!user) {
-      return Promise.reject();
-    }
+	return User.findOne({email}).then((user) => {
+		if(!user) {
+			return Promise.reject();
+		}
 
-    return new Promise((resolve, reject) => {
-      bcrypt.compare(password, user.password, (err, res) => {
-        if(res) {
-          resolve(user);
-        } else {
-          reject();
-        }
-      });
-    });
-  });
+		return new Promise((resolve, reject) => {
+			bcrypt.compare(password, user.password, (err, res) => {
+				if(res) {
+					resolve(user);
+				} else {
+					reject();
+				}
+			});
+		});
+	});
 };
 
 /**
@@ -154,25 +154,25 @@ UserSchema.statics.findByCredentials = function(email, password) {
  * @return {[undefined]}   Nothing is returned
  */
 UserSchema.pre('save', function (next) {
-  var user = this;
+	var user = this;
 
-  if(user.isModified('password')) {
-    var passToHash = user.password;
+	if(user.isModified('password')) {
+		var passToHash = user.password;
 
-    bcrypt.genSalt(10, (err, salt) => {
-      bcrypt.hash(passToHash, salt, (err, hash) => {
-        if(!err) {
-          user.password = hash;
-          next();
-        } else {
-          next('Error generating hash');
-        }
-      });
-    });
+		bcrypt.genSalt(10, (err, salt) => {
+			bcrypt.hash(passToHash, salt, (err, hash) => {
+				if(!err) {
+					user.password = hash;
+					next();
+				} else {
+					next('Error generating hash');
+				}
+			});
+		});
 
-  } else {
-    next();
-  }
+	} else {
+		next();
+	}
 });
 
 let User = mongoose.model('User', UserSchema);
